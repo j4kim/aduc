0d125eac6d3ab80a8692882bdd086ef77bac8071
Joaquim Perez <jivkim@gmail.com>
2021-11-14 20:27:37 +0100

    store created_at on reply

--- reply.php
+++ reply.php
@@ -9,7 +9,8 @@ setcookie("user_name", $_POST['name'], time()+60*60*24*365);
 $data->replies[] = [
   'reply' => $_POST['reply'],
   'name' => $_POST['name'],
-  'user_id' => $_COOKIE["user_id"]
+  'user_id' => $_COOKIE["user_id"],
+  'created_at' => time()
 ];
 
 $data->updated_at = time();
