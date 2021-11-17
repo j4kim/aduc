c965269501ce5b5f6211a0e9c85eb28c35257822
Joaquim Perez <jivkim@gmail.com>
2021-11-17 09:36:03 +0100

    The theme (light or dark) can be passed as an url argument

--- question.php
+++ question.php
@@ -16,7 +16,7 @@ foreach($data->replies as $reply) {
 ?>
 
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="fr" data-theme="<?= @$_GET['theme'] ?>">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
