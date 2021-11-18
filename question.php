a74df8d3f100d01c2e43f085b1b99ecca23cdf1c
Joaquim Perez <jivkim@gmail.com>
2021-11-18 10:53:33 +0100

    remove variable theme feature

--- question.php
+++ question.php
@@ -18,7 +18,7 @@ $repliesCount = count($data->replies);
 ?>
 
 <!DOCTYPE html>
-<html lang="fr" data-theme="<?= @$_GET['theme'] ?>">
+<html lang="fr">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
