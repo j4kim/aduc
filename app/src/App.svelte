1207e7316a0c65106318ba19c24a9db43475f921
Joaquim Perez <jivkim@gmail.com>
2021-11-21 16:19:37 +0100

    add svelte-navigator

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -1,9 +1,17 @@
 <script>
+	import { Router, Link, Route } from "svelte-navigator"
 </script>
 
-<main>
-	<h1>Hello!</h1>
-</main>
+<Router>
+	<Route path="/">
+		<h1>Hello</h1>
+		<Link to="/question">question</Link>
+	</Route>
+	<Route path="/question">
+		<h1>Question</h1>
+		<Link to="/">une-question.ch</Link>
+	</Route>
+</Router>
 
 <style>
 </style>
